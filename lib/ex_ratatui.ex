90e5427bd6e5edad537e29138c93db8ac356f303
Mauricio Cassola <mauricass19@gmail.com>
2026-02-16 17:10:09 +0100

    chore: initial commit

---
+++ lib/ex_ratatui.ex
@@ -0,0 +1,18 @@
+defmodule ExRatatui do
+  @moduledoc """
+  Documentation for `ExRatatui`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExRatatui.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_ratatui_test.exs
@@ -0,0 +1,8 @@
+defmodule ExRatatuiTest do
+  use ExUnit.Case
+  doctest ExRatatui
+
+  test "greets the world" do
+    assert ExRatatui.hello() == :world
+  end
+end
