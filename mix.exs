8b663ed7bc5c0d0f2d48175303644deb7a2b032b
Mauricio Cassola <mauricass19@gmail.com>
2026-02-17 18:31:08 +0100

    ci: add GitHub Actions workflow

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ExRatatui.MixProject do
     [
       app: :ex_ratatui,
       version: "0.1.0",
-      elixir: "~> 1.18",
+      elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       docs: docs()
