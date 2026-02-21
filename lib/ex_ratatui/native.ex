f3d849a9bf2ce8071833ff8ce4236b0a5f06f648
Mauricio Cassola <mauricass19@gmail.com>
2026-02-21 19:58:32 +0100

    fix: remove freebsd NIF target and update CI runners

--- lib/ex_ratatui/native.ex
+++ lib/ex_ratatui/native.ex
@@ -18,7 +18,6 @@ defmodule ExRatatui.Native do
       x86_64-apple-darwin
       x86_64-pc-windows-gnu
       x86_64-pc-windows-msvc
-      x86_64-unknown-freebsd
       x86_64-unknown-linux-gnu
       x86_64-unknown-linux-musl
     ),
